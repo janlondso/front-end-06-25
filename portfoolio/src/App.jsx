561099e9818b1fa662864f9009281751583ed432
janlondso <75534056+janlondso@users.noreply.github.com>
2025-06-04 23:15:35 +0300

    portfolio vol2

--- portfoolio/src/App.jsx
+++ portfoolio/src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import {Link, Route, Routes} from 'react-router-dom'
+import { Link, Route, Routes } from 'react-router-dom'
 import Work from './pages/Work'
 import Hobbies from './pages/Hobbies'
 import Courses from './pages/Courses'
@@ -12,15 +12,15 @@ function App() {
       <div className="rectangle"></div>
 
       <div className="navigation-pictures">
-        <Link className='main-link' to="work">
+        <Link className="main-link" to="work">
           <img src="https://images.unsplash.com/photo-1611162617213-7d7a39e9b1d7?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8YnJhbmRzfGVufDB8fDB8fHww" alt="" />
           <p>Tööde lehele</p>
         </Link>
-        <Link className='main-link' to="hobbies"> 
+        <Link className="main-link" to="hobbies"> 
           <img src="https://images.unsplash.com/photo-1508599589920-14cfa1c1fe4d?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGJyYW5kc3xlbnwwfHwwfHx8MA%3D%3D" alt="" />
           <p>Hobide lehele</p>
         </Link>
-        <Link className='main-link' to="courses">
+        <Link className="main-link" to="courses">
           <img src="https://images.unsplash.com/photo-1614231125961-38323d6c485b?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8YnJhbmRzfGVufDB8fDB8fHww" alt="" />
           <p>Kususte lehele</p>
         </Link>
@@ -34,10 +34,10 @@ function App() {
         <Route path='courses' element={ <Courses />} />
       </Routes>
 
-      <a href="#" className="facebook-button">
-        <img src="/facebook.png" alt="" />
-        <span>Facebook</span>
-      </a>
+        <a href="https://facebook.com/jee" className="facebook-button">
+          <img src="/facebook.png" alt="Facebook logo" />
+          <span>Facebook</span>
+        </a>
 
     </div>
   )
