0b2463522915e5ad0a0af0d59237da70c8de6d15
janlondso <75534056+janlondso@users.noreply.github.com>
2025-06-04 23:25:45 +0300

    portfolio kususe tekst

--- portfoolio/src/App.jsx
+++ portfoolio/src/App.jsx
@@ -22,7 +22,7 @@ function App() {
         </Link>
         <Link className="main-link" to="courses">
           <img src="https://images.unsplash.com/photo-1614231125961-38323d6c485b?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8YnJhbmRzfGVufDB8fDB8fHww" alt="" />
-          <p>Kususte lehele</p>
+          <p>Kursuste lehele</p>
         </Link>
       </div>
 
