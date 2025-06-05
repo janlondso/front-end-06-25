d2b5dc3618487392f2a009b730359bb03c4d88c4
janlondso <75534056+janlondso@users.noreply.github.com>
2025-06-05 18:43:22 +0300

    uudised/meist/kontakt data

--- uudised/src/pages/Kontakt.jsx
+++ uudised/src/pages/Kontakt.jsx
@@ -1,22 +1,31 @@
+import { useState } from "react";
+
 function Kontakt() {
+
+    const [n2itaTelKristiine, muudaN2itaTelKristiine] = useState(false);
+    const [n2itaTelYlemiste, muudaN2itaTelYlemiste] = useState(false);
+    const [n2itaTelTasku, muudaN2itaTelTasku] = useState(false);
+
     return ( 
-    <>
-        <div><h2>See on kontaktide leht, nähtav localhost: 3000/kontakt aadressil</h2></div>
-        <div><p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eni</p></div>
-        <br />
-        <div><p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eni</p></div>
-        <div><p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eni</p></div>
-        <div><p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eni</p></div>
-        <br />
-        <div><p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eni</p></div>
-        <div><p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eni</p></div>
-        <div><p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eni</p></div>
-        <br />
-        <div><p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eni</p></div>
-        <div><p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eni</p></div>
-        <div><p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eni</p></div>
-    </>
+        <div>
+            <div>See on kontaktide leht, nähtav localhost: 3000/kontakt aadressil</div>
+            <div>Võta meiega ühendust</div>
+            <br />
+            <div onClick={() => muudaN2itaTelKristiine(!n2itaTelKristiine)}>Kristiine keskus</div>
+            {n2itaTelKristiine && <div>+37257 777 777</div>}
+            <div>Endla 45</div>
+            <br />
+            <div onClick={() => muudaN2itaTelYlemiste(!n2itaTelYlemiste)}>Ülemiste keskus</div>
+            {n2itaTelYlemiste && <div>+37256 666 666</div>}
+            <div>Suur-sõjamäe 4</div>
+            <br />
+            <div onClick={() => muudaN2itaTelTasku(!n2itaTelTasku)}>Tasku keskus</div>
+           {n2itaTelTasku && <div>+37255 555 555</div>}
+            <div>Turu 2</div>
+        </div>
+
      );
 }
 
 export default Kontakt;
+
--- uudised/src/pages/Meist.jsx
+++ uudised/src/pages/Meist.jsx
@@ -1,25 +1,32 @@
+import { useState } from "react";
+
 function Meist() {
+    const [kontakt, n2itaKontakt] = useState("");
+
     return ( 
-    <>
-        <div><h2>See on meist leht, nähtav localhost: 3000/meist aadressil</h2></div>
+    <div>
         <div>See on meist leht, nähtav localhost: 3000/meist aadressil</div>
+        <div>Võta meiega ühendust</div>
         <br />
-        <div>See on meist leht, nähtav localhost: 3000/meist aadressil</div>
-        <div>See on meist leht, nähtav localhost: 3000/meist aadressil</div>
-        <div>See on meist leht, nähtav localhost: 3000/meist aadressil</div>
+        <div>Arvo Pärt</div>
+        <div>Uudisteklippide taustamuusika</div>
+        <button onClick={() => n2itaKontakt('+37258 888 888')}>Võta ühendust</button>
         <br />
-        <div>See on meist leht, nähtav localhost: 3000/meist aadressil</div>
-        <div>See on meist leht, nähtav localhost: 3000/meist aadressil</div>
-        <div>See on meist leht, nähtav localhost: 3000/meist aadressil</div>
+        <div>Kelly Sildaru</div>
+        <div>Püstolreporter</div>
+        <button onClick={() => n2itaKontakt('+37256 999 999')}>Võta ühendust</button>
         <br />
-        <div>See on meist leht, nähtav localhost: 3000/meist aadressil</div>
-        <div>See on meist leht, nähtav localhost: 3000/meist aadressil</div>
-        <div>See on meist leht, nähtav localhost: 3000/meist aadressil</div>
+        <div>Lennart Meri</div>
+        <div>President</div>
+        <button onClick={() => n2itaKontakt('+37256 111 111')}>Võta ühendust</button>
         <br />
-        <div>See on meist leht, nähtav localhost: 3000/meist aadressil</div>
-        <div>See on meist leht, nähtav localhost: 3000/meist aadressil</div>
-        <div>See on meist leht, nähtav localhost: 3000/meist aadressil</div>
-    </>
+        <div>Jaagup Kreem</div>
+        <div></div>
+        <div>Välisturgude spetsialist</div>
+        <button onClick={() => n2itaKontakt('+372 52 222 222')}>Võta ühendust</button>
+        <br /> <br />
+        { kontakt !== "" && <div>Tema kontakt: {kontakt} </div>}
+    </div>
      );
 }
 
