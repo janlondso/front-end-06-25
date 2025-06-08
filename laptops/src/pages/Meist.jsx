59df1ad894a4f37a3d4431a10107c84e28dae0df
janlondso <75534056+janlondso@users.noreply.github.com>
2025-06-08 17:30:11 +0300

    laptops meist page

--- laptops/src/pages/Meist.jsx
+++ laptops/src/pages/Meist.jsx
@@ -8,9 +8,9 @@ const [message, maaraMessage] = useState("Vali mõni tegevus");
   return (
     <div>
       <div>{message}</div>
-      <button onClick={() => {maaraMessage("Selleks saada e-mail jobs@html-css.com")}}>Kandideeri tööle</button>
-      <button onClick={() => {maaraMessage("Meil on 10 töötajat, kelle info ilmub veebilehele lähiajal")}}>Vaata meie töötajaid</button>
-      <button onClick={() => {maaraMessage("Ühenduse võtmiseks mine lehele Kontakt")}}>Võta meiega ühendust</button>
+      <button onClick={() => maaraMessage("Selleks saada e-mail jobs@html-css.com")}>Kandideeri tööle</button>
+      <button onClick={() => maaraMessage("Meil on 10 töötajat, kelle info ilmub veebilehele lähiajal")}>Vaata meie töötajaid</button>
+      <button onClick={() => maaraMessage("Ühenduse võtmiseks mine lehele Kontakt")}>Võta meiega ühendust</button>
     </div>
   )
 }
