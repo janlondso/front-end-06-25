27a63ff06a7b28bcb3c0ef7295b64732752a20b0
janlondso <75534056+janlondso@users.noreply.github.com>
2025-06-10 08:40:32 +0300

    portfoolio contact-page update

--- portfoolio/src/pages/Contact.jsx
+++ portfoolio/src/pages/Contact.jsx
@@ -1,35 +1,25 @@
 import { useState } from "react"
 
-
 function Contact() {
-    const [keel, setKeel] = useState("et");
-
-    const vahetaKeel = () => {    
-        if(keel === "et"){
-            setKeel("en");
-        }  else {
-            setKeel("et");
-        } 
+    const [city, setCity] = useState("Tallinn");
+    const [mobile, setMobile] = useState("55 555 555");
+    const [email, setEmail] = useState("oih@oihoih.com");
+    const [sonum, setSonum] = useState("Muuda inglise keelseks");
+    
+    const changeData = () => {    
+        setCity("London")
+        setMobile("77 777 777")
+        setEmail("london@london.uk")
+        setSonum("Change to Estonian")
     }
 
   return (
-    <div>
-        {keel === "et" && 
         <div>
-            <div>Tallinn</div>
-            <div>55 555 555</div>
-            <div>oih@oihoih.com</div>
-            <button onClick={vahetaKeel}>Muuda inglise keelseks</button>
-        </div>}
-
-        { keel === "en" &&
-            <div>
-            <div>London</div>
-            <div>80 800 800</div>
-            <div>london@london.com</div>
-            <button onClick={vahetaKeel}>Turn to Estonian</button>
-        </div>}
-    </div>
+            <div>{city}</div>
+            <div>{mobile}</div>
+            <div>{email}</div>
+            <button onClick={changeData}>{sonum}</button>
+        </div>
 )
     }
 
