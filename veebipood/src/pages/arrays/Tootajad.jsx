9c5a7445a5f4ea956233efea0d9a416b069dbfb3
janlondso <75534056+janlondso@users.noreply.github.com>
2025-06-19 20:17:56 +0300

    Update tootajad.jsx

--- veebipood/src/pages/arrays/Tootajad.jsx
+++ veebipood/src/pages/arrays/Tootajad.jsx
@@ -1,10 +1,27 @@
+import { useState } from "react";
 import ArraysHome from "./ArraysHome"
 
 
 function Tootajad() {
+
+  const tootajad = ["Meelis", "Toomas", "Diana" ,"Aavo", "Eve", "Mati", "Mart", "Linda", "Siiri", "Ivo"];
+  const [personal, setPersonal] = useState(tootajad);
+
+  const sortAtoZ = () => {
+    personal.sort((a, b) => a.localeCompare(b));
+    setPersonal(personal.slice());
+  }
+  const reset = () => {
+    setPersonal(tootajad);
+  }
+
   return (
     <div>
         <ArraysHome />
+        <div>Kokku on: {personal.length}</div>
+        <button onClick={reset}>Reset</button>
+        <button onClick={sortAtoZ}>sort A to Z</button>
+        {personal.map(persoon => <div key={persoon}>{persoon}</div>)}
     </div>
   )
 }
