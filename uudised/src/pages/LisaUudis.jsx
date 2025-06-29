efd3208f004271a1be9af70941748ed29b098294
janlondso <75534056+janlondso@users.noreply.github.com>
2025-06-29 13:15:15 +0300

    MuudaUudis.jsx

--- uudised/src/pages/LisaUudis.jsx
+++ uudised/src/pages/LisaUudis.jsx
@@ -1,18 +1,18 @@
 import { useRef } from "react"
 
 function LisaUudis() {
-  const uudiseRef = useRef();
+  const uudisedRef = useRef();
 
   const lisaUusUudis = () => {
     const uudised = JSON.parse(localStorage.getItem("uudised")) || [];
-    uudised.push(uudiseRef.current.value);
+    uudised.push(uudisedRef.current.value);
     localStorage.setItem("uudised", JSON.stringify(uudised));
   }
 
   return (
     <div>
         <label>Sisesta uus uudis</label> <br />
-        <input ref={uudiseRef} type="text" /> <br />
+        <input ref={uudisedRef} type="text" /> <br />
         <button onClick={lisaUusUudis}>Lisa uudis</button>
     </div>
   )
--- uudised/src/pages/MuudaUudis.jsx
+++ uudised/src/pages/MuudaUudis.jsx
@@ -1,10 +1,24 @@
+import { useRef } from 'react';
 import { useParams } from 'react-router-dom'
 
 function MuudaUudis() {
     const { index } = useParams();
+    const uudisRef = useRef();
+    const uudisedLS = JSON.parse(localStorage.getItem("uudised")) || [];
+    const leitudUudis = uudisedLS[index]; // const found = array[i]
+
+    const muuda = () => {
+      uudisedLS[index] = uudisRef.current.value; // asendan array index-i vana uudise uue vastu
+      // kui oleks objekt -> uudisedLS[index] = {"key1": value1, "key2": value2}
+      localStorage.setItem("uudised", JSON.stringify(uudisedLS)); // uuendus LS-is, kus uudisedLS on uus v22rtus
+    }
 
   return (
-    <div>MuudaUudis</div>
+    <div>
+      <label>Uudis</label> <br />
+      <input ref={uudisRef} type="text" defaultValue={leitudUudis} /> <br />
+      <button onClick={muuda}>Muuda</button>
+    </div>
   )
 }
 
