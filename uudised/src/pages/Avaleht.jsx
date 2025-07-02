8d6947038c29d6ee98cea73710336db6501c99ad
janlondso <75534056+janlondso@users.noreply.github.com>
2025-07-02 15:00:22 +0300

    uudised/Avalehtjsx

--- uudised/src/pages/Avaleht.jsx
+++ uudised/src/pages/Avaleht.jsx
@@ -1,13 +1,29 @@
+import { useState } from 'react';
 import './Avaleht.css'
+import { useEffect } from 'react';
 
 function Avaleht() {
+    const [postitused, uuenaPostitused] = useState([]);
+    //uef - kutsub effecti
+    useEffect(() => {
+      fetch("https://jsonplaceholder.typicode.com/posts")
+        .then(response => response.json())
+        .then(json => uuenaPostitused(json))
+
+    }, []);
     return (  
-    <>
         <div>
             <h2>See on avaleht, nähtav localhost: 3000 aadressil</h2>
             <img className="image-avaleht" src="https://images.unsplash.com/photo-1495020689067-958852a7765e?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8bmV3c3xlbnwwfHwwfHx8MA%3D%3D" alt="" />
+            {postitused.map(element =>
+              <div key={element.id}>
+                <div><i>{element.userId}</i></div>
+                <div><u>{element.id}</u></div>
+                <div><b>{element.title}</b></div>
+                <div>{element.body}</div>
+              </div>
+            )}
         </div>
-    </> 
     );
 }
 
