5581785022ffba1c3031e7edd5b84840056d0ec1
janlondso <75534056+janlondso@users.noreply.github.com>
2025-07-18 19:09:44 +0300

    my-portfolio/ NavBar, StarBackground, Home, NavBar.jsx

--- Samples/my-portfolio_toggle_white_and_black/src/pages/Home.jsx
+++ Samples/my-portfolio_toggle_white_and_black/src/pages/Home.jsx
@@ -1,23 +1,15 @@
-import NavBar from "../components/NavBar"
 import ThemeToggle from "../components/ThemeToggle"
-import StarBackground from "@/components/StarBackground"
-import HeroSection from "../components/HeroSection"
 
 function Home() {
   return (
     <div className="min-h-screen bg-background text-foreground overflow-x-hidden"> 
       {/* Theme toggle */}
         <ThemeToggle />
-
       {/* Background Effects */}
-        <StarBackground />
 
       {/* Navbar */}
-        <NavBar />
+
       {/* Main Content */}
-      <main>
-        <HeroSection />
-      </main>
 
       {/* Footer */}
 
