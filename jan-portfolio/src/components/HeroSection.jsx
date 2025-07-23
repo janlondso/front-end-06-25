7c798af9f6afd2b6c4fc411e7547e19006178fde
janlondso <75534056+janlondso@users.noreply.github.com>
2025-07-23 11:36:54 +0300

    jan-portfolio/ HeroSection.jsx

--- jan-portfolio/src/components/HeroSection.jsx
+++ jan-portfolio/src/components/HeroSection.jsx
@@ -1,10 +1,10 @@
-
+import "../css/hero.css"
 
 function HeroSection() {
 
   return (
     <section className="hero-section">
-      
+      <div className=" box shadow-box"></div>
     </section>
   )
 }
