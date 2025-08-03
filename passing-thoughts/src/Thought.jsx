da520ea38ff7b105176156cc98fb742c132096bc
janlondso <75534056+janlondso@users.noreply.github.com>
2025-08-03 17:43:53 +0300

    project passing-thoughts

---
+++ passing-thoughts/src/Thought.jsx
@@ -0,0 +1,32 @@
+import { useEffect } from "react";
+
+export function Thought(props) {
+  const { thought, removeThought } = props;
+
+  useEffect(() => {
+    const timeUp = setTimeout(() => {
+    removeThought(thought.id);
+    }, thought.expiresAt - Date.now());
+
+    return () => {
+      clearTimeout(timeUp);
+    }
+  }, [thought, removeThought]);
+
+  const handleRemoveClick = () => {
+    removeThought(thought.id);
+  };
+
+  return (
+    <li className="Thought">
+      <button
+        aria-label="Remove thought"
+        className="remove-button"
+        onClick={handleRemoveClick}
+      >
+        &times;
+      </button>
+      <div className="text">{thought.text}</div>
+    </li>
+  );
+}
